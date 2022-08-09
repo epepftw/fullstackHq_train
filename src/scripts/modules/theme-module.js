53df21196c923c1b19c1852683bddac4f7414f01
epepftw <ipinggxlirangg@gmail.com>
2022-08-09 17:41:05 +0800

    Fixed Changes for Reviews

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -14,6 +14,19 @@ AppName.Modules.ThemeModule = (function () {
       pagination: {
         el: '.swiper-pagination',
         clickable: true
+      },breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
       },
     });
   };
